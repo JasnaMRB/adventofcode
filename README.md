d7ff34d83663ef20c43e691270f2b57c86a69bc8
Jasna <jasna@datasembly.com>
2021-12-03 11:04:31 -0500

    2021 Day 1, part 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code Solutions
+
+Probably written in Scala but may try to branch out to other languages, too.
---
+++ src/main/scala/year2021/Day1.scala
@@ -0,0 +1,24 @@
+package year2021
+
+object Day1 {
+
+  def getDepths(filePath: String): List[Int] = {
+    val bufferedSource = io.Source.fromFile(filePath)
+    try {
+      bufferedSource.getLines().map(_.toInt).toList
+    } finally bufferedSource.close()
+  }
+
+  def getNumLargerMeasurements(depths: List[Int]): Int = {
+    depths.tail.foldLeft((0,depths.head)){
+      case ((acc, prevDepth), depth) =>
+        val nextAcc = if (depth > prevDepth) acc + 1 else acc
+        (nextAcc, depth)
+    }._1
+  }
+}
+
+/**
+ * val depths = Day1.getDepths
+ * Day1.getNumLargerMeasurements(depths)
+ **/
